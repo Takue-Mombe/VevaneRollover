9d83e43d9266a31e7253f677742e89cbd3ce0354
Takue <h220462g@hit.ac.zw>
2024-11-27 16:12:25 +0200

    commit

--- app/Http/Controllers/fileController.php
+++ app/Http/Controllers/fileController.php
@@ -14,20 +14,32 @@ class fileController extends Controller
         ]);
 
         //Store FIle
+
+        //validating if the file has the correct parameters/extensions
         if($request->hasFile('file')){
 
+
             $file = $request->file('file');
+            //adding a timestamp to the filename to prevent it from being overwritten
             $fileName=time() . '_'. $file->getClientOriginalName();
+            //aves the file in the storage/app/public/uploads directory with the given name.
             $filePath=$file->storeAs('uploads',$fileName,'public');
 
 
 
+            //saves the file's metadata to the files table
             File::create([
+                //original file name
                'name'=>$file->getClientOriginalName(),
+               //file extension
                'type'=>$file->getClientOriginalExtension(),
+               //file path
                'path'=>$filePath,
             ]);
+            //Sends a JSON response back to the client confirming a successful upload.
+            return response()->json(['success'=>'File uploaded successfully.']);
         }
-
+        //an error message that is returned when the file fails to upload for some reason
+        return response()->json(['error'=>'File not uploaded.']);
     }
 }
